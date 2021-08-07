1171fe70b2038cb74a4b84d0c2aed8003056c452
chisom <princechekwas@gmail.com>
2021-08-07 14:00:56 +0100

    container orchestration and deployment

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -4,8 +4,9 @@
 
 export const environment = {
   production: false,
-  appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  appName: "Udagram",
+  apiHost:
+    "http://a651e591fed5843ed9a93eb8fa26a174-1549958647.us-east-2.elb.amazonaws.com:8080/api/v0",
 };
 
 /*
